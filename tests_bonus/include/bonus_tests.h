64280fa7b693109f90089f673653c081c7e02866
tosu <timo42@proton.me>
2024-02-03 23:45:28 +0100

    Makefile + main.c

---
+++ tests_bonus/include/bonus_tests.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_tests.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tosuman <timo42@proton.me>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 17:12:30 by tosuman           #+#    #+#             */
+/*   Updated: 2024/02/03 23:38:59 by tosuman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BONUS_TESTS_H
+# define BONUS_TESTS_H 1
+
+int	abort_launcher(void);
+int	alarm_launcher(void);
+int	fpe_launcher(void);
+int	illegal_launcher(void);
+
+#endif /* bonus_tests.h. */
---
+++ tests_bonus/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tosuman <timo42@proton.me>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/03 17:10:54 by tosuman           #+#    #+#             */
+/*   Updated: 2024/02/03 23:39:33 by tosuman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft/libft.h"
+#include "include/bonus_tests.h"
+
+int	main(void)
+{
+	abort_launcher();
+	alarm_launcher();
+	fpe_launcher();
+	illegal_launcher();
+	return (0);
+}
