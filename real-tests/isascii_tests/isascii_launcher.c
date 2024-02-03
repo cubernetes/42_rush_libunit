58830f0db09f8ccb9e78ff4df6316b4e60095ff1
Sysex89 <40394815+Sysex89@users.noreply.github.com>
2024-02-03 22:29:10 +0100

    more tests (#3)
    * fixing min tests hopefully -- have to double check what ft_min is actually supposed to returned with int_limits
    
    * some bonus setup
    
    * a bit of untangling
    
    * more tests

---
+++ real-tests/isascii_tests/isascii_launcher.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isascii_launcher.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvasilan <pvasilan@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 21:06:39 by tosuman           #+#    #+#             */
+/*   Updated: 2024/02/03 22:05:06 by pvasilan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../framework/libunit.h"
+#include "../../libft/libft.h"
+
+#define ROUTINE_NAME "ISASCII"
+
+int	char_test(void);
+int	num_test(void);
+int	space_test(void);
+int	null_test(void);
+
+int	isascii_launcher(void)
+{
+	t_ddeque	*tests;
+	int			ret;
+
+	tests = ddeque_init();
+	load_test(tests, "Ascii character test", char_test);
+	load_test(tests, "Number test", num_test);
+	load_test(tests, "Space test", space_test);
+	load_test(tests, "NULL test", null_test);
+	return (launch_tests(tests, ROUTINE_NAME));
+}
