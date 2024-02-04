6908d23fb038afe3a0bbe2abb43b5fa2aba9eff6
tosu <timo42@proton.me>
2024-02-04 20:56:24 +0100

    Delete and rename some files

--- real-tests/isdigit_tests/00_isdigit_launcher.c
+++ real-tests/isdigit_tests/00_isdigit_launcher.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isascii_launcher.c                                 :+:      :+:    :+:   */
+/*   00_isdigit_launcher.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvasilan <pvasilan@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 21:06:39 by tosuman           #+#    #+#             */
-/*   Updated: 2024/02/04 20:35:02 by tosuman          ###   ########.fr       */
+/*   Updated: 2024/02/04 20:53:12 by tosuman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../framework/libunit.h"
 #include "../../libft/libft.h"
 
-#define ROUTINE_NAME "ISASCII"
+#define ROUTINE_NAME "ISDIGIT"
 
-int	char_test(void);
-int	num_test(void);
-int	space_test(void);
-int	null_test(void);
+int	isdigit_char_test(void);
+int	isdigit_num_test(void);
+int	isdigit_space_test(void);
+int	isdigit_null_test(void);
 
-int	isascii_launcher(void)
+int	isdigit_launcher(void)
 {
 	t_ddeque	*tests;
 
 	tests = ddeque_init();
-	load_test(tests, "Ascii character test", char_test);
-	load_test(tests, "Number test", num_test);
-	load_test(tests, "Space test", space_test);
-	load_test(tests, "NULL test", null_test);
+	load_test(tests, "ASCII Character test", isdigit_char_test);
+	load_test(tests, "Number test", isdigit_num_test);
+	load_test(tests, "Space test", isdigit_space_test);
+	load_test(tests, "NULL test", isdigit_null_test);
 	return (launch_tests(tests, ROUTINE_NAME));
 }
